cb449299453179cb11fba35edfcae00e60c294ec
Hephyrius <20884880+Hephyrius@users.noreply.github.com>
2023-10-19 00:37:41 +0100

    create a basic omnitoken

---
+++ contracts/Omnipool/OmniToken.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.8.4;
+
+import { ERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
+import { Ownable } from "@openzeppelin/contracts-0.8/access/Ownable.sol";
+
+// import { console } from "hardhat/console.sol";
+
+contract OmniToken is ERC20, Ownable {
+
+    address public minter;
+
+    constructor() ERC20("OMNIPOOL Token", "Omni") {}
+
+    function setMinter(address _minter) onlyOwner external {
+        require( _minter != address(0), "!0");
+        minter = _minter;
+    }
+}
